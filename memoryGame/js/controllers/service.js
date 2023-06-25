588b13759e418f4747f96350db34a89f497e45bd
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-06-25 06:45:43 -0600

    creation of objects

---
+++ memoryGame/js/controllers/service.js
@@ -0,0 +1,6 @@
+export class Service {
+    constructor(controller) {
+        this.controller = controller;
+        this.container = document.createElement('div');
+    }
+}
--- memoryGame/js/index.js
+++ memoryGame/js/index.js
@@ -1,1 +1,9 @@
-console.log("App runing");
+'use strict';
+
+import { GameManager } from "./gameManager.js";
+
+window.addEventListener('load', init, false);
+
+function init() {
+    const gameManager = new GameManager();
+}
