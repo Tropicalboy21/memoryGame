28f8f1f42dc72fc26db9207532c827e6443d83c2
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-06-25 19:04:15 -0600

    new details

--- memoryGame/js/controllers/menu/menuView.js
+++ memoryGame/js/controllers/menu/menuView.js
@@ -3,5 +3,16 @@ import { View } from "../view.js";
 export class MenuView extends View {
     constructor(controller, parent) {
         super(controller, parent);
+        this.container.className = 'menuView';
+
+        var loginBtn = document.createElement('div');
+        loginBtn.className = 'menuView-btn';
+        this.container.appendChild(loginBtn);
+
+        var img = document.createElement('div');
+        img.className = 'icon';
+        loginBtn.appendChild(img);
+
+
     }
 }
