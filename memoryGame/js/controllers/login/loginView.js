c51617d8eb4779e8dbf429e270510bef78983177
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-07-01 08:55:13 -0600

    login details

--- memoryGame/js/controllers/login/loginView.js
+++ memoryGame/js/controllers/login/loginView.js
@@ -1,14 +1,19 @@
 import { View } from "../view.js";
+import { div, p } from "../../libs/html.js"
 
 export class LoginView extends View {
     constructor(controller, parent) {
         super(controller, parent);
         this.container.className = 'loginView';
 
-        var loginBtn = document.createElement('div');
-        this.container.appendChild(loginBtn);
-        loginBtn.innerHTML = 'play';
-        loginBtn.className = 'btn';
+        var welcomeTxt = div({ className: 'welcome-text' }, this.container);
+        var loginCaption = p({ innerHTML: 'enter username to play', className: 'login-caption' }, this.container)
+        var inputLogin = document.createElement('input');
+        this.container.appendChild(inputLogin);
+        inputLogin.className = 'input-login';
+        inputLogin.setAttribute('id', 'inputLogin');
+        document.getElementById('inputLogin').placeHolder = 'username';
+        var loginBtn = div({ innerHTML: 'play', className: 'btn' }, this.container);
     }
 
 }
--- memoryGame/js/controllers/play/playView.js
+++ memoryGame/js/controllers/play/playView.js
@@ -1,8 +1,10 @@
+import { div } from "../../libs/html.js";
 import { View } from "../view.js";
 
 export class PlayView extends View {
     constructor(controller, parent) {
         super(controller, parent);
         this.container.className = 'playView';
+
     }
 }
