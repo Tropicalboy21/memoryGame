80d9e3f52435b5fec6fe8d469ee53b69b531bdc2
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-07-04 06:13:10 -0600

    gulp

--- memoryGame/gulpfile.js
+++ memoryGame/gulpfile.js
@@ -1,3 +1,13 @@
+const browserSync = require("browser-sync");
+const server = browserSync.create();
+
+
 exports.play = () => {
     console.log("GULP - running play function");
+    browserSync.init({
+        server: {
+            baseDir: "."
+        }
+    });
+
 }
