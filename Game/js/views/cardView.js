741b19f0faebb0601c7288a71714c04a04873ccc
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-07-21 19:35:36 -0600

    css improvements

--- Game/js/views/cardView.js
+++ Game/js/views/cardView.js
@@ -7,8 +7,28 @@ export class CardView extends View {
         this.card = card;
         this.container.className = 'cardView-container';
         this.iconContainer = div({ className: 'cardView cardView-hidden' }, this.container);
+        this.container.onclick = this.onSelected.bind(this);
 
         this.iconContainer.innerHTML = this.card.icon;
 
+        window.addEventListener('show-card-on-selected', (event) => {
+            this.showOnSelected();
+        });
+
+
+    }
+
+    onSelected() {
+        this.card.isSelected = true;
+
+        var event = new CustomEvent('card-selected', {
+            detail: {
+                card: this.card,
+            },
+            bubbles: true,
+            cancelable: true,
+            composed: false,
+        });
+        this.container.dispatchEvent(event);
     }
 } 
